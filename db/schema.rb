99c21c30139484b859d7b14a8eb315c495c6f427
Yuta <ny_bigapplejr@yahoo.co.jp>
2017-07-17 03:08:17 +0000

    201707171208

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170710083146) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
+    t.string   "name"
+    t.string   "email"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

58a2ea8ed1c628aa3fb06163af4e0f4d53ab2bb2
Yuta <ny_bigapplejr@yahoo.co.jp>
2017-08-13 03:46:11 +0000

    20170813

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,7 @@ class CommentsController < ApplicationController
         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
           unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
         })
+        format.js { render :index }
       else
         format.html { render :new }
       end
